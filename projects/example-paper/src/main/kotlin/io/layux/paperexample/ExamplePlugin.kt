70af3981e7d19072b6f6b4e81b347be76369c1a3
Fernando Lopez <misterquestions.mx@gmail.com>
2023-02-23 23:17:49 -0800

    chore: remove random code added
    Signed-off-by: Fernando Lopez <misterquestions.mx@gmail.com>

--- projects/example-paper/src/main/kotlin/io/layux/paperexample/ExamplePlugin.kt
+++ projects/example-paper/src/main/kotlin/io/layux/paperexample/ExamplePlugin.kt
@@ -28,38 +28,4 @@ class ExamplePlugin : JavaPlugin() {
     override fun onDisable() {
         logger.info("ExamplePlugin disabled!")
     }
-
-    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
-        if (command.name == "rotateglass") {
-            if (sender is Player) {
-                val block = sender.getTargetBlock(null, 5)
-
-                if (block.type == Material.GLASS) {
-                    // Get the current direction the block is facing
-                    val directional = block.blockData as Directional
-
-                    // Determine the new direction based on the current direction
-                    val newDirection = when (directional.facing) {
-                        BlockFace.NORTH -> BlockFace.EAST
-                        BlockFace.EAST -> BlockFace.SOUTH
-                        BlockFace.SOUTH -> BlockFace.WEST
-                        BlockFace.WEST -> BlockFace.NORTH
-                        else -> directional.facing // In case the current direction is not one of the cardinal directions
-                    }
-
-                    // Set the new direction for the block
-                    directional.setFacingDirection(newDirection)
-                    block.state.update()
-
-                    sender.sendMessage("Glass block rotated")
-                } else {
-                    sender.sendMessage("You must be looking at a glass block to use this command")
-                }
-            } else {
-                sender.sendMessage("You must be a player to use this command")
-            }
-            return true
-        }
-        return super.onCommand(sender, command, label, args)
-    }
 }
