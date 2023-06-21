4dea7ba3c8185e8b80754ed1b747168e62d1c8b8
Fernando Lopez <fernando.lopez@layux.io>
2023-06-21 12:35:39 -0600

    feat: update example common entrypoint

---
+++ projects/example-fabric/src/main/kotlin/io/layux/example/ExampleMod.kt
@@ -0,0 +1,29 @@
+package io.layux.example
+
+import net.fabricmc.api.ModInitializer
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+/**
+ *
+ *
+ * @project template-minecraft
+ * @package io.layux.fabricexample
+ * @author MisterQuestions <misterquestions@layux.io>
+ * @date 12/04/2023 02:37 AM
+ *
+ * © 2023 Layux LLC. All rights reserved.
+ */
+class ExampleMod : ModInitializer {
+    companion object {
+        const val MOD_ID = "fabric_example"
+        val LOGGER: Logger = LoggerFactory.getLogger(MOD_ID)
+    }
+
+    /**
+     * Runs the mod initializer.
+     */
+    override fun onInitialize() {
+        LOGGER.info("Hello Fabric world!")
+    }
+}
