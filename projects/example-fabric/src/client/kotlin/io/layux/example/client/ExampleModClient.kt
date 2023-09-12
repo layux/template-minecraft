77cba20fd3ef82a519a72d6784ac9529dc0fd857
MisterQuestions <fernando.lopez@layux.io>
2023-09-12 08:47:18 -0600

    feat: update fabric example

--- projects/example-fabric/src/client/kotlin/io/layux/example/client/ExampleModClient.kt
+++ projects/example-fabric/src/client/kotlin/io/layux/example/client/ExampleModClient.kt
@@ -1,23 +1,19 @@
 package io.layux.example.client
 
-import io.layux.example.ExampleMod
 import net.fabricmc.api.ClientModInitializer
 
 /**
+ * Entry point for client-side code.
  *
- *
+ * @author Fernando Lopez <fernando.lopez@layux.io>
  * @project template-minecraft
  * @package io.layux.example.client
- * @author MisterQuestions <misterquestions@layux.io>
- * @date 21/06/2023 01:22 PM
+ * @date 12/09/2023 8:03 AM
  *
  * © 2023 Layux LLC. All rights reserved.
  */
-class ExampleModClient : ClientModInitializer {
-    /**
-     * Runs the mod initializer on the client environment.
-     */
-    override fun onInitializeClient() {
-        ExampleMod.LOGGER.info("Initializing client...")
-    }
+object ExampleModClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
 }
--- projects/example-fabric/src/main/kotlin/io/layux/example/ExampleMod.kt
+++ projects/example-fabric/src/main/kotlin/io/layux/example/ExampleMod.kt
@@ -1,29 +1,25 @@
 package io.layux.example
 
 import net.fabricmc.api.ModInitializer
-import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 /**
+ * Entry point for common-side code.
  *
- *
+ * @author Fernando Lopez <fernando.lopez@layux.io>
  * @project template-minecraft
- * @package io.layux.fabricexample
- * @author MisterQuestions <misterquestions@layux.io>
- * @date 12/04/2023 02:37 AM
+ * @package io.layux.example.client.mixin
+ * @date 12/09/2023 8:03 AM
  *
  * © 2023 Layux LLC. All rights reserved.
  */
-class ExampleMod : ModInitializer {
-    companion object {
-        const val MOD_ID = "fabric_example"
-        val LOGGER: Logger = LoggerFactory.getLogger(MOD_ID)
-    }
+object ExampleMod : ModInitializer {
+    private val logger = LoggerFactory.getLogger("fabric-example")
 
-    /**
-     * Runs the mod initializer.
-     */
-    override fun onInitialize() {
-        LOGGER.info("Hello Fabric world!")
-    }
+	override fun onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		logger.info("Hello Fabric world!")
+	}
 }
